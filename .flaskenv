c21227e59369549ba560093df33e649d3398efce
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-10-05 19:13:41 -0400

    build: add redis dep and configure redis settings. (#7)

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,5 @@
 FLASK_APP=app.py
 FLASK_DEBUG=1
+DATABASE_URL=sqlite://
+REDIS_URL=redis://localhost:6379
+APP_NAME=InsuredDataAccessService
--- src/config/config.py
+++ src/config/config.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from sqlalchemy import QueuePool
+import os
+from config.extensions import cache
 
 
 class Config:
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_RECORD_QUERIES = True
@@ -15,10 +17,16 @@ class Config:
         "pool_pre_ping": True,
     }
 
+    CACHE_TYPE = "RedisCache"
+    CACHE_DEFAULT_TIMEOUT = 5
+    CACHE_REDIS_URL = os.getenv("REDIS_URL")
+    CACHE_KEY_PREFIX = os.getenv("APP_NAME")
+
 
 def create_app() -> Flask:
     """Application factory pattern."""
 
     app = Flask(__name__)
     app.config.from_object(Config)
+    cache.init_app(app)
     return app
