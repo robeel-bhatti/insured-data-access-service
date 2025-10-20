c63e97da381f1486eed9678de975e08afc163fa6
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-10-19 23:11:06 -0400

    chore: cleanup pyproject.toml.

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,11 @@ from sqlalchemy import pool
 from alembic import context
 from src.models.address import Base as Base1
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -38,7 +43,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.getenv("DATABASE_URL")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,8 +62,11 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    cfg = config.get_section(config.config_ini_section, {})
+    cfg["url"] = os.getenv("DATABASE_URL")
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        cfg,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
