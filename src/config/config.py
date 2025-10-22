140f135a594ba4ebd1d352a28e7c02bb959513f1
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-10-22 19:49:37 -0400

    fix: configure sqlalchemy and attach session to app object.

--- src/config/config.py
+++ src/config/config.py
@@ -1,26 +1,12 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import QueuePool
 import os
-from config.extensions import cache
 
 
 class Config:
     DEBUG = True
-
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-    SQLALCHEMY_RECORD_QUERIES = True
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool": QueuePool,
-        "pool_recycle": 3600,
-        "pool_pre_ping": True,
-    }
-
-    CACHE_TYPE = "RedisCache"
-    CACHE_DEFAULT_TIMEOUT = 5
-    CACHE_REDIS_URL = os.getenv("REDIS_URL")
-    CACHE_KEY_PREFIX = os.getenv("APP_NAME")
 
 
 def create_app() -> Flask:
@@ -28,5 +14,20 @@ def create_app() -> Flask:
 
     app = Flask(__name__)
     app.config.from_object(Config)
-    cache.init_app(app)
     return app
+
+
+def init_db(app: Flask) -> None:
+    options = {
+        "poolclass": QueuePool,
+        "pool_recycle": 3600,
+        "pool_pre_ping": True,
+    }
+
+    url = os.getenv("DATABASE_URL")
+    if url is None:
+        raise Exception("DATABASE_URL is not set")
+
+    engine = create_engine(url, **options)
+    session = sessionmaker(bind=engine)
+    app.session = session()
