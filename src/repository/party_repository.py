bd99b5d2dbe83ae61071428edd7f745fcf765a85
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-10-25 16:20:22 -0400

    feat: add repository layer. (#17)
    * feat: add abstract repo to decouple my repos from the ORM.
    
    * chore: add todos.
    
    * feat: create entity repos that follow the abstract repo contract.

---
+++ src/repository/party_repository.py
@@ -0,0 +1,20 @@
+from src.repository.abstract_repository import AbstractRepository
+from src.models.party import Party
+from sqlalchemy.orm import Session
+
+
+class PartyRepository(AbstractRepository[Party]):
+    def __init__(self, session: Session):
+        self.session = session
+
+    # def get_all(self, query: str) -> list[Party]:
+    #     pass
+
+    def get_by_id(self, id: int) -> Party:
+        return self.session.get_one(entity=Party, ident=id)
+
+    def add(self, entity: Party) -> None:
+        self.session.add(entity)
+
+    def delete(self, id: int) -> None:
+        self.session.delete(instance=Party)
