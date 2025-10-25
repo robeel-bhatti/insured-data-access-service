18093e24afeccfb16ddebae8755c7ec9cc71e5c7
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-10-25 00:21:11 -0400

    feat: add abstract repo to decouple my repos from the ORM.

---
+++ src/repository/abstract_repository.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class AbstractRepository(ABC, Generic[T]):
+    @abstractmethod
+    def get_all(self) -> list[T]:
+        pass
+
+    @abstractmethod
+    def get_by_id(self, id: int) -> T:
+        pass
+
+    @abstractmethod
+    def add(self, entity: T) -> T:
+        pass
+
+    @abstractmethod
+    def update(self, entity: T) -> T:
+        pass
+
+    @abstractmethod
+    def delete(self, id: int) -> T:
+        pass
