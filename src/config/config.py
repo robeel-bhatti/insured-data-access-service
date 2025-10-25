05e29cda5357c1c67937683a9913d4c0c6038e99
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-10-25 00:00:45 -0400

    feat: add health check endpoint. (#16)
    * build: add flask smorest for REST integration.
    
    * feat: add health check endpoint.
    
    * fix: add flask stubs for mypy.
    
    * chore: remove extra empty lines at EOF.

--- src/config/config.py
+++ src/config/config.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import QueuePool
 from redis import Redis
 import os
+from flask_smorest import Api
+from src.blueprints.health_check_blueprint import hc_blp
 
 
 class Config:
     DEBUG = True
+    API_TITLE = "My API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0.2"
 
 
 def create_app() -> Flask:
@@ -17,6 +22,8 @@ def create_app() -> Flask:
     app.config.from_object(Config)
     init_db(app)
     init_cache(app)
+    api = Api(app)
+    api.register_blueprint(hc_blp)
     return app
 
 
@@ -34,8 +41,9 @@ def init_db(app: Flask) -> None:
         )
 
     engine = create_engine(url, **options)
-    session = sessionmaker(bind=engine)
-    app.session = session()
+    session_factory = sessionmaker(bind=engine)
+    Session = scoped_session(session_factory)
+    app.session = Session()
 
 
 def init_cache(app: Flask) -> None:
