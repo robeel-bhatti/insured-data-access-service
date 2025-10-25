8358565297b1511502c64df79a3d984121e8d93c
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-10-25 16:16:31 -0400

    feat: create entity repos that follow the abstract repo contract.

--- src/repository/abstract_repository.py
+++ src/repository/abstract_repository.py
@@ -5,29 +5,18 @@ T = TypeVar("T")
 
 
 class AbstractRepository(ABC, Generic[T]):
-    @abstractmethod
-    def get_all(self) -> list[T]:
-        pass
+    # @abstractmethod
+    # def get_all(self, query: str) -> list[T]:
+    #     pass
 
     @abstractmethod
     def get_by_id(self, id: int) -> T:
         pass
 
     @abstractmethod
-    def add(self, entity: T) -> T:
+    def add(self, entity: T) -> None:
         pass
 
     @abstractmethod
-    def update(self, entity: T) -> T:
+    def delete(self, id: int) -> None:
         pass
-
-    @abstractmethod
-    def delete(self, id: int) -> T:
-        pass
-
-
-# TODO: Add party repository
-# TODO: Add address repository
-# TODO: Session object in repository or service?
-# TODO: Commit/Rollback in repository or service?
-# TODO: Add teardown handler to close all DB sessions
---
+++ src/repository/address_repository.py
@@ -0,0 +1,20 @@
+from src.models.address import Address
+from src.repository.abstract_repository import AbstractRepository
+from sqlalchemy.orm import Session
+
+
+class AddressRepository(AbstractRepository[Address]):
+    def __init__(self, session: Session):
+        self.session = session
+
+    # def get_all(self, query: str) -> list[Address]:
+    #     pass
+
+    def get_by_id(self, id: int) -> Address:
+        return self.session.get_one(entity=Address, ident=id)
+
+    def add(self, entity: Address) -> None:
+        self.session.add(entity)
+
+    def delete(self, id: int) -> None:
+        self.session.delete(instance=Address)
